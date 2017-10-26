83dac5794bf66838c4852853170a3d5f708d02f0
João Ferreira <jmnsferreira@gmail.com>
2017-10-26 22:16:16 +1100

    Uses defaultLogLevel after types update (#18)
    Signed-off-by: jmnsf <jmnsferreira@gmail.com>

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,7 +1,5 @@
 import * as log from 'loglevel'
 
-// FIXME The type signature for this function is wrong...
-// log.setDefaultLevel(log.levels.INFO)
-log.setLevel(log.levels.INFO)
+log.setDefaultLevel(log.levels.INFO)
 
 export default log
